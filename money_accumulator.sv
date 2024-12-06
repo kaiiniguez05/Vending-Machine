0f772ea18e68573e124331b212a932ba18c039c5
kaiiniguez05 <158511362+kaiiniguez05@users.noreply.github.com>
2024-12-05 22:58:50 -0800

    Update money_accumulator.sv

--- money_accumulator.sv
+++ money_accumulator.sv
@@ -1,16 +1,18 @@
-module accumulator(
-    input clk, LD, reset,
-    input [7:0] D,
+module accumulator (
+    input  clk,
+    input  LD,
+    input  reset,
+    input  [7:0] D,
+    input  update_total_money,
+    input  [7:0] remaining_money,
     output logic [7:0] Q = 0
-    );
-    
-    always_ff @ (posedge clk)
-    begin
+);
+    always_ff @(posedge clk) begin
         if (reset)
             Q <= 0;
+        else if (update_total_money)
+            Q <= remaining_money; // Load the remaining money
         else if (LD)
-            Q <= D + Q;
-         
-    
+            Q <= Q + D;
     end
 endmodule
